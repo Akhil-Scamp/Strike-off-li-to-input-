cdda5ea8b8ac20265dfb5d6edf87b46f6ae68e38
Akhil-Scamp <problamaticboy@gmail.com>
2022-06-27 21:49:59 +0530

    initial one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useEffect } from 'react'
 
-function App() {
+const App = () => {
+  const NZP = ["Trent","Daz","Tom","Kane"];
+   useEffect(()=>{
+    return () => handleToggle();
+  },[])
+  
+  const handleToggle = () => {
+    console.log("qwer")
+    const list = document.querySelectorAll("li");
+    for (let pic of list){
+      const input = document.createElement('input');
+      pic.addEventListener("click", ()=>{
+        
+        input.setAttribute('type', "text");
+        pic.replaceWith(input);
+        input.focus();
+        let make = " "
+        input.setAttribute('value',pic.textContent);
+        input.onchange = function (event){
+          console.log(event.target.value);
+          make = event.target.value
+          input.setAttribute('value',make);
+        }
+        input.addEventListener('blur', () => {
+          const li = document.createElement("li")
+          li.innerHTML = make;
+          input.replaceWith(li)
+          });
+        }); 
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {
+          NZP.map((x)=>(<li key = {x} className= "listItem">{x}</li>))
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
